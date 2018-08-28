61a1db5ff8b7c545b322e687d07542d8e87c4822
Pavel Březina <pbrezina@redhat.com>
2018-08-28 10:46:44 +0200

    Setup machines one by one
    This will make the provisioning more stable since bringing up five machines
    at once is very costly on system performance.

--- setup.sh
+++ setup.sh
@@ -1,53 +1,26 @@
 #!/bin/bash
 
+success_or_die() {
+    if [ $1 -ne 0 ]; then
+        echo $2
+        exit 1
+    fi
+}
+
 echo "1. Preparing host..."
 ansible-playbook -i "localhost," -c local "./provision/prepare-host.yml"
+success_or_die $? "Unable to provision host machine!"
 
-if [ $? -ne 0 ]; then
-    echo "Unable to provision host machine!"
-    exit 1
-fi
+# It is better to bring machines up one by one to avoid timeout issues
+echo "2. Bringing up guests one by one..."
+for HOST in ad ad-child ipa ldap client; do
+    vagrant up $HOST
+    success_or_die $? "Unable to bring up host: $HOST!"
+done
 
-echo "2. Bringing up guests..."
-# Windows machines sometimes timeout when starting up in parallel
-# so we start them first in sequence.
-
-vagrant up ad
-
-if [ $? -ne 0 ]; then
-    echo "Unable to bring ad up!"
-    exit 1
-fi
-
-vagrant up ad-child
-
-if [ $? -ne 0 ]; then
-    echo "Unable to bring ad-child up!"
-    exit 1
-fi
-
-vagrant up
-
-if [ $? -ne 0 ]; then
-    echo "Unable to bring guests up!"
-    exit 1
-fi
-
-echo "3. Remove old enrollment data"
-rm -fr ./shared-enrollment/ad ./shared-enrollment/ldap ./shared-enrollment/ipa
-
-if [ $? -ne 0 ]; then
-    echo "Unable to remove old enrollment data!"
-    exit 1
-fi
-
-echo "4. Provisioning guests..."
+echo "3. Provisioning guests..."
 ./provision.sh
-
-if [ $? -ne 0 ]; then
-    echo "Unable to provision guests!"
-    exit 1
-fi
+success_or_die $? "Unable to provision guests!"
 
 echo "Guest machines are ready."
 echo "Use 'vagrant ssh client' to ssh into client machine."
